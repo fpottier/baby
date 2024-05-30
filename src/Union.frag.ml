16fa771eff00cc3905fbe8614d76be2cd4a7bc93
François Pottier <francois.pottier@inria.fr>
2024-05-30 19:33:52 +0200

    In [union], test which tree seems smaller only once at the root.

--- src/Union.frag.ml
+++ src/Union.frag.ml
@@ -1,7 +1,13 @@
 (* The union algorithm proposed by BFS is improved (or complicated)
    with two features of OCaml's Set library:
    - the subtree that seems smaller is split (this is a heuristic);
-   - if one subtree is a singleton then [union] degenerates to [add]. *)
+   - if one subtree is a singleton then [union] degenerates to [add].
+
+   Whereas OCaml tests which subtree seems smaller at every step in
+   the recursive function, we do so only once at the beginning. This
+   avoids code duplication and seems to work just as well. (Because
+   the trees are balanced, in the recursive function, the property
+   that [t1] is (roughly) the smaller tree is preserved.) *)
 
 let rec union (t1 : tree) (t2 : tree) : tree =
   match VIEW(t1), VIEW(t2) with
@@ -9,19 +15,24 @@ let rec union (t1 : tree) (t2 : tree) : tree =
       t2
   | _, LEAF ->
       t1
-  | NODE(l1, k1, r1), NODE(l2, k2, r2) ->
+  | NODE(_, k1, _), NODE(l2, k2, r2) ->
+      if is_singleton t1 then add k1 t2 else
+      let l1, _, r1 = split k2 t1 in
+      let l = union l1 l2
+      and r = union r1 r2 in
+      node l k2 r
+
+let union (t1 : tree) (t2 : tree) : tree =
+  match VIEW(t1), VIEW(t2) with
+  | LEAF, _ ->
+      t2
+  | _, LEAF ->
+      t1
+  | NODE(_, _, _), NODE(_, _, _) ->
       if seems_smaller t2 t1 then
-        if is_singleton t2 then add k2 t1 else
-        let l2, _, r2 = split k1 t2 in
-        let l = union l1 l2
-        and r = union r1 r2 in
-        node l k1 r
+        union t2 t1
       else
-        if is_singleton t1 then add k1 t2 else
-        let l1, _, r1 = split k2 t1 in
-        let l = union l1 l2
-        and r = union r1 r2 in
-        node l k2 r
+        union t1 t2
 
 let rec inter (t1 : tree) (t2 : tree) : tree =
   match VIEW(t1), VIEW(t2) with
