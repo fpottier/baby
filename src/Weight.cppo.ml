5243d62c90cfc35c083c818da27963a13edf1f3a
François Pottier <francois.pottier@inria.fr>
2024-06-03 10:29:22 +0200

    Comment.

--- src/Weight.cppo.ml
+++ src/Weight.cppo.ml
@@ -30,6 +30,7 @@ module[@inline] Make (E : OrderedType) = struct
     | TNode of { l : tree; v : key; r : tree; w : int }
 
   (* This macro destructs a tree [t] that is known not to be a leaf. *)
+  (* TODO use this macro in Height.cppo.ml, too *)
 
   #define DESTRUCT(t,tl,tv,tr) \
     match t with \
