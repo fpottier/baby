7037fc029147d9397efaffafeb0618c17f757cb5
François Pottier <francois.pottier@inria.fr>
2024-06-03 22:58:53 +0200

    Move [equal] next to [compare]. Remove redundant fast path.

--- src/Readers.frag.ml
+++ src/Readers.frag.ml
@@ -201,7 +201,7 @@ let to_seq (t : tree) : key Seq.t =
 
 (* -------------------------------------------------------------------------- *)
 
-(* Comparisons. *)
+(* Comparison. *)
 
 (* Instead of using two numerations for the trees [t1] and [t2], one could
    perform a recursive traversal of [t1], while consuming an enumeration of
@@ -211,3 +211,17 @@ let to_seq (t : tree) : key Seq.t =
 let compare (t1 : tree) (t2 : tree) : int =
   if t1 == t2 then 0 else (* fast path *)
   Enum.(compare (enum t1) (enum t2))
+
+(* -------------------------------------------------------------------------- *)
+
+(* Equality. *)
+
+(* Equality can be implemented in several ways. E.g., [equal t1 t2] could be
+   implemented in one line by [subset t1 t2 && subset t2 t1] or also in one
+   line by [is_empty (xor t1 t2)]. (The latter idea could be optimized, so
+   as to avoid actually constructing the tree [xor t1 t2] in memory.) Some
+   experiments suggest that either of these approaches is more expensive
+   than the following approach, which is based on [compare]. *)
+
+let equal t1 t2 =
+  compare t1 t2 = 0
