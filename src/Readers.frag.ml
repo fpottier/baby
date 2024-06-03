81da160ca1b073d368b0ae5e0ddf97557b6244fc
François Pottier <francois.pottier@inria.fr>
2024-06-03 23:17:10 +0200

    Comment.

--- src/Readers.frag.ml
+++ src/Readers.frag.ml
@@ -203,10 +203,10 @@ let to_seq (t : tree) : key Seq.t =
 
 (* Comparison. *)
 
-(* Instead of using two numerations for the trees [t1] and [t2], one could
-   perform a recursive traversal of [t1], while consuming an enumeration of
-   [t2]. I have tested and benchmarked this variant: it allocates less memory,
-   and can be faster, but can also be about twice slower. *)
+(* Instead of using enumerations of the trees [t1] and [t2], one could perform
+   a recursive traversal of [t1], while consuming an enumeration of [t2]. I
+   have benchmarked this variant: it allocates less memory, and can be faster,
+   but can also be about twice slower. *)
 
 let compare (t1 : tree) (t2 : tree) : int =
   if t1 == t2 then 0 else (* fast path *)
