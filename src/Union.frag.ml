469f0f7875076ce506a88e119909a2586d5c724e
François Pottier <francois.pottier@inria.fr>
2024-06-05 11:08:21 +0200

    Exchange the definitions of [union] and [inter] in the source code.

--- src/Union.frag.ml
+++ src/Union.frag.ml
@@ -1,45 +1,5 @@
 (* -------------------------------------------------------------------------- *)
 
-(* Union. *)
-
-(* The union algorithm proposed by BFS is improved (or complicated)
-   with two features of OCaml's Set library:
-   - the subtree that seems smaller is split (this is a heuristic);
-   - if one subtree is a singleton then [union] degenerates to [add].
-
-   Whereas OCaml tests which subtree seems smaller at every step in
-   the recursive function, we do so only once at the beginning. This
-   avoids code duplication and seems to work just as well. (Because
-   the trees are balanced, in the recursive function, the property
-   that [t1] is (roughly) the smaller tree is preserved.) *)
-
-let rec union (t1 : tree) (t2 : tree) : tree =
-  match VIEW(t1), VIEW(t2) with
-  | LEAF, _ ->
-      t2
-  | _, LEAF ->
-      t1
-  | NODE(_, k1, _), NODE(l2, k2, r2) ->
-      if is_singleton t1 then add k1 t2 else
-      let l1, r1 = split13 k2 t1 in
-      let l = union l1 l2
-      and r = union r1 r2 in
-      join l k2 r
-
-let union (t1 : tree) (t2 : tree) : tree =
-  match VIEW(t1), VIEW(t2) with
-  | LEAF, _ ->
-      t2
-  | _, LEAF ->
-      t1
-  | NODE(_, _, _), NODE(_, _, _) ->
-      if seems_smaller t2 t1 then
-        union t2 t1
-      else
-        union t1 t2
-
-(* -------------------------------------------------------------------------- *)
-
 (* Intersection. *)
 
 (* This is the simple, elegant version of [inter] given by BFS.
@@ -105,6 +65,46 @@ let inter t1 t2 =
     inter t2 t1
   else
     inter t1 t2
+
+(* -------------------------------------------------------------------------- *)
+
+(* Union. *)
+
+(* The union algorithm proposed by BFS is improved (or complicated)
+   with two features of OCaml's Set library:
+   - the subtree that seems smaller is split (this is a heuristic);
+   - if one subtree is a singleton then [union] degenerates to [add].
+
+   Whereas OCaml tests which subtree seems smaller at every step in
+   the recursive function, we do so only once at the beginning. This
+   avoids code duplication and seems to work just as well. (Because
+   the trees are balanced, in the recursive function, the property
+   that [t1] is (roughly) the smaller tree is preserved.) *)
+
+let rec union (t1 : tree) (t2 : tree) : tree =
+  match VIEW(t1), VIEW(t2) with
+  | LEAF, _ ->
+      t2
+  | _, LEAF ->
+      t1
+  | NODE(_, k1, _), NODE(l2, k2, r2) ->
+      if is_singleton t1 then add k1 t2 else
+      let l1, r1 = split13 k2 t1 in
+      let l = union l1 l2
+      and r = union r1 r2 in
+      join l k2 r
+
+let union (t1 : tree) (t2 : tree) : tree =
+  match VIEW(t1), VIEW(t2) with
+  | LEAF, _ ->
+      t2
+  | _, LEAF ->
+      t1
+  | NODE(_, _, _), NODE(_, _, _) ->
+      if seems_smaller t2 t1 then
+        union t2 t1
+      else
+        union t1 t2
 
 (* -------------------------------------------------------------------------- *)
 
