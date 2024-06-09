b874a0025e353e626012c5ff3c487651e4d4c425
François Pottier <francois.pottier@inria.fr>
2024-06-09 20:59:13 +0200

    Document the signature [BST].

--- src/Signatures.ml
+++ src/Signatures.ml
@@ -3,39 +3,97 @@ module type OrderedType = sig
   val compare : t -> t -> int
 end
 
+(**The signature [BST] describes the interface offer by the balancing code.
+   The remaining operations on binary search tree are built on top of this
+   interface, and are oblivious to the balancing criterion. *)
 module type BST = sig
+
+  (**Keys, or elements. *)
   type key
+
+  (**Balanced binary search trees. *)
   type tree
+
+  (**A view on a balanced binary search tree indicates whether this tree
+     is a leaf or a node, and, if it is a node, gives access to its left
+     child, its key, and its right child. A view does not give access to
+     balancing information, such as the tree's height or weight. *)
   type view =
     | Leaf
     | Node of tree * key * tree
+
+  (**[view] turns a tree into a view. *)
   val view : tree -> view
 
-  (* TODO val make : view -> tree *)
+  (* In the reverse direction, one could imagine a conversion function
+     [make : view -> tree]. In order to avoid a memory allocation, we
+     replace this function with a constant [leaf] and a function [join]. *)
+
+  (**[leaf] is the empty tree; a leaf. *)
   val leaf : tree
+
+  (**[join l v r] expects a subtree [l], a key [v], and a subtree [r] such
+     that [l < v < r] holds. It returns a new tree whose elements are the
+     elements of [l], [v], and [r]. If needed, it performs rebalancing, so the
+     key [v] is not necessarily found at the root of the new tree. *)
   val join : tree -> key -> tree -> tree
-  (* BFS write: "the cost of [join] must be proportional to the difference in
-     ranks of two trees, and the rank of the result of a join must be at most
-     one more than the maximum rank of the two arguments". *)
+  (* Regarding computational complexity, BFS write: the cost of [join] must be
+     proportional to the difference in ranks of two trees, and the rank of the
+     result of a join must be at most one more than the maximum rank of the
+     two arguments. *)
+
+  (* The following functions are not part of the minimal interface proposed
+     by BFS. Exposing these functions allow us to gain efficiency in various
+     situations. *)
+
+  (**[join_neighbors l v r] is analogous to [join l v r]. Like [join], it
+     requires [l < v < r]. Furthermore, it assumes that the trees [l] and [r]
+     have been obtained by taking two siblings in a well-formed tree and by
+     adding or removing one element in one of them. *)
   val join_neighbors : tree -> key -> tree -> tree
-    (* assumes that the two trees [l] and [r] were siblings in a well-formed tree
-       and that one of them has been disturbed by adding or removing one element. *)
 
+  (**[join_weight_balanced l v r] is analogous to [join l v r]. Like [join],
+     it requires [l < v < r]. Furthermore, it assumes that the weights of the
+     trees [l] and [r] differ by at most one. *)
   val join_weight_balanced : tree -> key -> tree -> tree
-    (* assumes that the weights of the two trees differ by at most one *)
 
+  (**If the weight of a tree can be determined in constant time, then [weight
+     t] returns the weight of the tree [t]. If the weight of a tree cannot be
+     efficiently determined, then it is acceptable for [weight] to always
+     return zero. The function [weight] is used to implement fast paths in
+     subset and equality tests: it must be the case that [subset t1 t2]
+     implies [weight t1 <= weight t2]. *)
   val weight : tree -> int
-    (* If weight cannot be determined in constant time,
-       OK to always return 0. Used in fast paths in subset and equality tests. *)
 
-  (* TODO not part of the minimal interface: *)
+  (**[singleton x] constructs a tree whose sole element is [x]. *)
   val singleton : key -> tree
+
+  (**[doubleton x y] requires [x < y]. It constructs a tree whose elements are
+     [x] and [y]. *)
   val doubleton : key -> key -> tree
+
+  (**[tripleton x y z] requires [x < y < z]. It constructs a tree whose
+     elements are [x], [y], and [z]. *)
   val tripleton : key -> key -> key -> tree
+
+  (**[is_singleton t] determines whether the tree [t] is a singleton, that
+     is, whether it contains just one element. *)
   val is_singleton : tree -> bool
+
+  (**[seems_smaller t1 t2] indicates which of the trees [t1] and [t2] seems
+     smaller, based on height or weight. This function is used as part of a
+     heuristic choice, so no correctness obligation bears on it; its
+     postcondition is [true]. *)
   val seems_smaller : tree -> tree -> bool
+
+  (**[siblings t1 t2] determines whether the trees [t1] and [t2] can be
+     siblings in a valid tree. It is used in debugging assertions only. *)
   val siblings : tree -> tree -> bool
+
+  (**[check t] checks that the tree [t] is well-formed: that is, [t] is a
+     balanced binary search tree. This function is used while testing only. *)
   val check : tree -> unit
+
 end
 
 module type SET = sig
