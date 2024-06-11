2ca55e195bad1c0866dc998c70cc2f3df78e5523
François Pottier <francois.pottier@inria.fr>
2024-06-11 19:37:01 +0200

    Move a couple macro definitions.

--- src/Common.frag.ml
+++ src/Common.frag.ml
@@ -1,3 +1,11 @@
+(* [EMPTY(t)] determines whether the tree [t] is empty, that is, a leaf. *)
+
+#define EMPTY(t)        (match VIEW(t) with LEAF -> true | _ -> false)
+
+(* [BOTH_EMPTY(l,r)] determines whether the trees [l] and [r] are both empty. *)
+
+#define BOTH_EMPTY(l,r) (EMPTY(l) && EMPTY(r))
+
 #include "Basics.frag.ml"
 #include "MinMax.frag.ml"
 #include "Add.frag.ml"
--- src/Union.frag.ml
+++ src/Union.frag.ml
@@ -1,11 +1,3 @@
-(* [EMPTY(t)] determines whether the tree [t] is empty, that is, a leaf. *)
-
-#define EMPTY(t)        (match VIEW(t) with LEAF -> true | _ -> false)
-
-(* [BOTH_EMPTY(l,r)] determines whether the trees [l] and [r] are both empty. *)
-
-#define BOTH_EMPTY(l,r) (EMPTY(l) && EMPTY(r))
-
 (* -------------------------------------------------------------------------- *)
 
 (* Intersection. *)
