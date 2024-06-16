eb48c35aeffd7c8ab0529d74e5abcc00f29e0efa
François Pottier <francois.pottier@inria.fr>
2024-06-16 21:49:37 +0200

    Run [make headache].

--- benchmark/BinarySearchTree/IO.ml
+++ benchmark/BinarySearchTree/IO.ml
@@ -1,3 +1,15 @@
+(******************************************************************************)
+(*                                                                            *)
+(*                                   Bistro                                   *)
+(*                                                                            *)
+(*                       François Pottier, Inria Paris                        *)
+(*                                                                            *)
+(*       Copyright 2024--2024 Inria. All rights reserved. This file is        *)
+(*       distributed under the terms of the GNU Library General Public        *)
+(*       License, with an exception, as described in the file LICENSE.        *)
+(*                                                                            *)
+(******************************************************************************)
+
 (* [exhaust channel] reads all of the data that's available on [channel].
    It does not assume that the length of the data is known ahead of time.
    It does not close the channel. *)
