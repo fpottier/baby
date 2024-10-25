874dae691484c8812cd9fa422108cb94333a113f
François Pottier <francois.pottier@inria.fr>
2024-10-25 17:21:41 +0200

    Remove Cardinal.frag.ml, which was #included only once.

--- src/Height.cppo.ml
+++ src/Height.cppo.ml
@@ -59,7 +59,22 @@ module[@inline] Make (E : OrderedType) = struct
 
   (* The cardinal of a tree cannot be determined in constant time. *)
 
-  #include "Cardinal.frag.ml"
+  (* This is a linear-time [cardinal] function. *)
+
+  let constant_time_cardinal =
+    false
+
+  let rec cardinal accu (t : tree) : int =
+    match t with
+    | TLeaf ->
+        accu
+    | TNode { l; r; _ } ->
+        let accu = accu + 1 in
+        let accu = cardinal accu l in
+        cardinal accu r
+
+  let cardinal (t : tree) : int =
+    cardinal 0 t
 
   (* [siblings l r] checks that [l] and [r] could be siblings in a valid
      tree. [neighbors l r] checks that [l] and [r] could be neighbors,
