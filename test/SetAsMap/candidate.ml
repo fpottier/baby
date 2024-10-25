f3a8689fe5e737e8b734cf0fe50aa259ae9bffa4
François Pottier <francois.pottier@inria.fr>
2024-10-25 15:17:47 +0200

    Remove comments.

--- test/SetAsMap/candidate.ml
+++ test/SetAsMap/candidate.ml
@@ -1,5 +1,5 @@
 (* This module implements a map (whose keys and values are integers)
-   as a set of pairs. *)
+   as a set of pairs. It is currently very incomplete. *)
 
 module Key = Int
 
@@ -22,10 +22,6 @@ let empty =
 let singleton k v =
   Set.singleton (k, v)
 
-(* TODO need variants of [Set.mem] and [Set.find] and [Set.remove] that take
-   the partial application [compare x] as an argument, instead of [x]
-   itself. *)
-
 let dummy = 0
 
 let mem k m =
@@ -45,32 +41,14 @@ let has_binding k v m =
   | exception Not_found -> false
   | v' -> v' == v
 
-(* TODO need a variant of [Set.add] which always replaces the old value
-   with the new value, and preserves physical equality if the two values
-   are physically equal *)
-
 let add k v m =
   if has_binding k v m then
     m
   else
     Set.add (k, v) (remove k m)
 
-(* TODO need a variant of [Set.union] where the new element is computed
-   as a function of the previous two elements? but the function must
-   promise to return an element that is equivalent to the two elements
-   that it receives (dynamic check). *)
-
 let cardinal =
   Set.cardinal
 
 let is_empty =
   Set.is_empty
-
-(* TODO need a variant of [Set.equal] that is parameterized with an
-   equality function on values *)
-
-(* TODO need a variant of [Set.compare] that is parameterized with an
-   ordering function on values *)
-
-(* TODO to implement enumerations on maps, need a variant of [Set.Enum.from]
-   that takes [compare x] as an argument. This may be related to [find_first]. *)
